53b8999dc363c62a88dee6625372ce01aff9e8b8
nwrim <nakwon.rim@gmail.com>
2021-08-24 20:07:36 +0900

    pushing to ask empirica slack

--- client/game/TaskStimulus.jsx
+++ client/game/TaskStimulus.jsx
@@ -1,32 +1,27 @@
 import React from "react";
-import { TimeSync } from "meteor/mizzao:timesync";
-import moment from "moment";
 
 export default class TaskStimulus extends React.Component {   
-
-  removeWord = item => {
-    const {stage, player} = this.props;
-    
-    stage.set('wordList', stage.get('wordList').filter(l => l !== item));
-}
-
-
   showList = list => {
   const { round, stage, player } = this.props;
-        //onClick={() => this.removeWord(item)}>
-        //onClick={() => stage.set('wordList', stage.get('wordList').filter(l => l !== item))}>
     return (
     <ol>
       {list.map(function(item) {
         return <li key={item}> {item} <button type="button" 
-        onClick={() => this.removeWord(item)}>
-
+        onClick={(item) => this.removeWord(item)}>
+        {/*onClick={() => stage.set('wordList', stage.get('wordList').filter(l => l !== item))}>*/}
         Remove
         </button> </li>;
       })}
     </ol>
     )
   }
+  
+  removeWord = item => {
+    const {stage, player} = this.props;
+    
+    stage.set('wordList', stage.get('wordList').filter(l => l !== item));
+}
+
 
  
   render() {
