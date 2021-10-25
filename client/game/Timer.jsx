a8c3caeda271b18dea00a7714e710bc146e6159d
nwrim <nwrim@users.noreply.github.com>
2021-10-24 19:41:14 -0500

    update timer so that is shows min:sec format

--- client/game/Timer.jsx
+++ client/game/Timer.jsx
@@ -2,6 +2,13 @@ import { StageTimeWrapper } from "meteor/empirica:core";
 import React from "react";
 
 class timer extends React.Component {
+  secondsToTime= (e) => {
+    const m = Math.floor(e % 3600 / 60).toString().padStart(2,'0')
+    const s = Math.floor(e % 60).toString().padStart(2,'0');
+
+  return (m + ':' + s);
+  }
+
   render() {
     const { remainingSeconds } = this.props;
 
@@ -11,11 +18,11 @@ class timer extends React.Component {
     } else if (remainingSeconds <= 10) {
       classes.push("lessThan10");
     }
-
+  
     return (
       <div className={classes.join(" ")}>
         <h2 className="fw-bold">Timer</h2>
-        <span className="seconds">{remainingSeconds}</span>
+        <span className="seconds">{this.secondsToTime(remainingSeconds)}</span>
       </div>
     );
   }
