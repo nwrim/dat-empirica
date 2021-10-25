ac3b9f4cb5f63ee7966dd41fd5be51eed292bd03
nwrim <nwrim@users.noreply.github.com>
2021-10-24 19:42:03 -0500

    update instruction inside the game

--- client/game/PlayerProfile.jsx
+++ client/game/PlayerProfile.jsx
@@ -6,7 +6,7 @@ export default class PlayerProfile extends React.Component {
       <div className="alert alert-secondary player-profile" role="alert">
         <h2 className="fw-bold">Rules</h2>
         <ol>
-          <li>Only <b>single words</b> in English (no spaces).</li>
+          <li>Only <b>single words</b> in English.</li>
           <li>Only <b>nouns</b> (e.g., things, objects, concepts).</li>
           <li><b>No proper nouns</b> (e.g., no specific people or places).</li>
           <li><b>No specialised vocabulary</b> (e.g., no technical terms).</li>
